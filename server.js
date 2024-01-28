feabe1ac1bbd7ff73e38c0c443564665a53737f2
Juan <mendozajuan007@gmail.com>
2024-01-28 17:10:46 -0400

    cambiando server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url')
 const next = require('next')
  
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
+const hostname = 'https://cocipeniel.org.ve'
 const port = 3000
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
